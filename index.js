c6b115de54ae352a67bfbec097275c91196328cd
Woshiajuana <zhigang.chen@owulia.com>
2019-05-23 16:00:55 +0800

    tijiao

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
+#!/usr/bin/env node
 
 // 引入依赖
-let program = require('commander');
-let package = require('./package.json');
-// let init = require('./bin/init');
-// let list = require('./bin/list');
+const program = require('commander');
 
 // 定义版本和参数依赖
 program
-  .usage('<command>')
-  .version(package.version, '-v, --version');
-
-program.command('init (template)')
+  .version(require('./package.json').version, '-v, --version')
+  .usage('<command> [options]');
+program
+   .command('init (template)')
   .description("Generate a new project")
   .alias('i')
-  .action(function(template){
-    console.log('init',template)
-    // init(template);
+  .action(function (template) {
+    require('./lib/init')();
   });
-
-program.command('list')
+program
+  .command('list')
   .description("List all the templates")
   .alias('l')
-  .action(function(template){
-    console.log('list',template)
-    // list();
+  .action(function(){
+    require('./lib/list');
   });
 
 program.parse(process.argv);
