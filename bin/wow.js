02758c244c8b929b5282c4f69ec028051dface94
Woshiajuana <zhigang.chen@owulia.com>
2020-10-23 11:57:13 +0800

    subcode

--- bin/wow.js
+++ bin/wow.js
@@ -1,2 +1,11 @@
 #!/usr/bin/env node
 
+const program = require("commander");
+
+program
+    .version(require('../package').version, '-v, --version')
+    .usage('<command> [options]')
+    .command('init', 'generate a new project from a template')
+;
+
+program.parse(process.argv);
